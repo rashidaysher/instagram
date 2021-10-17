f4567a7d9637da4a37c53be39963a0610a787c95
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-10-17 15:25:39 +0300

    modified the views

--- insta/models.py
+++ insta/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Bio(models.Models):
+class Bio(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     phone = models.CharField(max_length=20, blank=True)
