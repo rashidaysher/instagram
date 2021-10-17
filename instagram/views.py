98dd2a7cd2a0fdd584bd2aafd29faae336e50c80
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-10-17 16:49:38 +0300

    added the forms file

---
+++ instagram/views.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import SignUpForm
+from insta.models import Bio
+
+def signup(request):
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = SignUpForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user = User.objects.create_user(username=username,
+                                            password=password,
+                                            first_name=first_name,
+                                            last_name=last_name,
+                                            email=email)
+            bio= Bio.objects.create(user=user)
+
+            return HttpResponseRedirect('/accounts/login')
+
+    else:
+        form = SignUpForm()
+
+    return render(request, 'registration/signup.html', {'form': form})
