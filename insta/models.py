50915f4a21f95cfc90b4bbc82b281fadccaaf214
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-10-17 14:29:10 +0300

    defined the bio model

--- insta/models.py
+++ insta/models.py
@@ -3,4 +3,9 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Bio(models.Models):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    biography = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    profile_pic = models.ImageField(blank=True, null=True)
 
