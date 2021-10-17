002534878c24a606f866ec77ad23a0b5e08076bb
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-10-17 15:05:04 +0300

    defined the bioform class

--- insta/forms.py
+++ insta/forms.py
@@ -1,3 +1,22 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, fields
 from .models import Post, Bio
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['image', 'location', 'caption']
+
+class BioForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""  
+
+    dp = forms.ImageField(required=False)
+    username = forms.CharField(max_length=500,required=True)
+    first_name = forms.CharField(max_length=500, required=False)
+    last_name = forms.CharField(max_length=500, required=False)
+    phone = forms.CharField(max_length=20, required=False)
+    bio= forms.CharField(required=False, widget=forms.Textarea())
+
