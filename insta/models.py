91a283d202fb63f4e56719773dd198df6ec258b5
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-10-17 15:01:28 +0300

    added the post model

--- insta/models.py
+++ insta/models.py
@@ -5,7 +5,55 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Bio(models.Models):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    biography = models.TextField(blank=True)
-    phone_number = models.CharField(max_length=20, blank=True)
-    profile_pic = models.ImageField(blank=True, null=True)
+    bio = models.TextField(blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    dp = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def split_bio(self):
+        return self.bio.split("\n")
+
+class Post(models.Model):
+    author = models.ForeignKey(Bio, on_delete=models.CASCADE)
+    location = models.CharField(max_length=500, null=True, blank=True)
+    caption = models.CharField(max_length=500, null=True, blank=True)
+    time_posted = models.DateTimeField(default=timezone.now)
+    image = models.ImageField()
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-time_posted']
+
+    def __str__(self):
+        return self.caption
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_linked = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    description = models.CharField(max_length=500)
+    comment_posted_on = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return "Comment by {} on {}".format(self.user.username, self.post_linked.caption)
+
+    class Meta:
+        ordering = ('-comment_posted_on',)
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_linked = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return 'User :{} Liked {} Post '.format(self.user.username,self.post_linked.caption)
+
+  
+
+
+
+
+
+
 
