b324c63e9bd80a714a32d3ee564f4b8bd6c30c17
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-10-18 12:31:59 +0300

    altered the paths

--- insta/urls.py
+++ insta/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index,name="index"),
     path('user/<str:username>', views.user_bio,name="user_bio"),
     path('top_comment', views.top_comment,name="top_comment"),
-    path('like/<int:postid>',views.likes_post,name='likes_post'),
-    path('create/post', views.add_post, name='add_post'),
-    path('edit/<str:username>', views.edit_profile, name='edit_profile'),
+    path('likes/<int:postid>',views.likes_post,name='likes_post'),
+    path('create/post', views.add_post, name='add_post'),  
+    path('edit/user/<str:username>', views.edit_profile, name='edit_profile'),
 ]
